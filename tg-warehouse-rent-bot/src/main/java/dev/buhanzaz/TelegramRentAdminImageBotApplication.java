368ef818ac8ee4d2420560194a64130ca68b0955
Buhanzaz <Buhanza@gmail.com>
2024-12-07 14:56:23 +0300

    init

---
+++ tg-warehouse-rent-bot/src/main/java/dev/buhanzaz/TelegramRentAdminImageBotApplication.java
@@ -0,0 +1,13 @@
+package dev.buhanzaz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramRentAdminImageBotApplication {
+    // TODO Реализовать автоматическую регистрацию вебхука
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramRentAdminImageBotApplication.class, args);
+    }
+
+}
