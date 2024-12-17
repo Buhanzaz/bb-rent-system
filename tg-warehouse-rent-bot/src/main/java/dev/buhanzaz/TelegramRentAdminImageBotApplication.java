e81613e8ff4bb203b939955b26bc2429b0bc1f1e
Buhanzaz <Buhanza21@yandex.ru>
2024-12-17 12:31:32 +0300

    refactor: spring application run class

--- tg-warehouse-rent-bot/src/main/java/dev/buhanzaz/TelegramRentAdminImageBotApplication.java
+++ tg-warehouse-rent-bot/src/main/java/dev/buhanzaz/TelegramRentAdminImageBotApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TelegramRentAdminImageBotApplication {
-    // TODO Реализовать автоматическую регистрацию вебхука
     public static void main(String[] args) {
         SpringApplication.run(TelegramRentAdminImageBotApplication.class, args);
     }
-
 }
