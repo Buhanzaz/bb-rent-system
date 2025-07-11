a26bb5e3afe44502a9649eb1fd4fe5ef4f2db50a
Buhanzaz <Buhanzaz@yandex.ru>
2025-07-11 07:04:58 +0300

    init

---
+++ src/main/java/deb/buhanzaz/BbRentSystemApplication.java
@@ -0,0 +1,13 @@
+package deb.buhanzaz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbRentSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BbRentSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/deb/buhanzaz/BbRentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package deb.buhanzaz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbRentSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
